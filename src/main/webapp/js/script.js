20fa77508748b99cfc6c3f9633ab9fe78c2714fa
Gowdamohan2 <gowdamogan221@gmail.com>
2023-11-11 12:00:13 +0530

    giooo

---
+++ src/main/webapp/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    var hamburger=$(".hamburger");
+    var navLink=$(".menu");
+    $(hamburger).click(function(){
+        $(navLink).toggleClass("hide")
+    });
+    $('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+			}
+		}
+	});
+})
+
